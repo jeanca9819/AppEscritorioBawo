24b5dd961c8e10fa6d3428156a51b345e9e82a86
Jean Carlo Valenciano <jeancavm1303@hotmail.com>
2021-02-13 12:00:54 -0600

    Creación del proyecto, creación de las capas e inicio de las ventanas inicio de sesión y principal (Solo GUI)

---
+++ src/manejobd/ManejoBD.java
@@ -0,0 +1,13 @@
+package manejobd;
+
+import gui.Login;
+
+public class ManejoBD {
+
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setLocationRelativeTo(null);
+        login.setVisible(true);
+    }
+    
+}
